b616b2bedb854587a4d8b477c42aa62fc359f23b
Rony <50582705+rony9707@users.noreply.github.com>
2026-04-30 17:05:33 +0530

    Agnibha/vercel release (#3)
    * Feature/image upload
    
    Imagekit Upload Release
    
    * Vercel Release--#1
    
    * Vercel fix 1
    
    * UUID removed
    
    ---------
    
    Co-authored-by: Soumya Raj Sarkar <117941685+Snaju003@users.noreply.github.com>

--- src/common/functions/common.functions.ts
+++ src/common/functions/common.functions.ts
@@ -1,4 +1,4 @@
-import { devLogger } from "@utils/logger.util";
+import { devLogger } from "../../utils/logger.util";
 import { Request, Response } from "express";
 
 // Function to run before running API logic, to check for API key or other preconditions
